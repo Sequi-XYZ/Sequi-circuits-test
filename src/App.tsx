5fc46d411ba8b02674fb789c3d8e03fc44763629
Joss <joss@aztecprotocol.com>
2022-09-20 11:52:41 +0100

    - downgrade to react-scripts 4 - install and setup sdk - add basic sdk init example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  AztecSdk,
+  createAztecSdk,
+  JsonRpcProvider,
+  RollupProviderStatus,
+  SdkFlavour,
+} from "@aztec/sdk";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [sdk, setSdk] = useState<AztecSdk>();
+  useEffect(() => {
+    const jsonRpcProvider = new JsonRpcProvider(
+      "https://mainnet-fork.aztec.network:8545"
+    );
+    createAztecSdk(jsonRpcProvider, {
+      serverUrl: "https://api.aztec.network/aztec-connect-dev/falafel",
+      flavour: SdkFlavour.PLAIN,
+      minConfirmation: 1,
+    }).then(setSdk);
+  }, []);
+  const [remoteStatus, setRemoteStatus] = useState<RollupProviderStatus>();
+  useEffect(() => {
+    if (sdk) {
+      sdk.getRemoteStatus().then(setRemoteStatus);
+    }
+  }, [sdk]);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +42,11 @@ function App() {
         >
           Learn React
         </a>
+        {!sdk && <p>Initialising Aztec SDK...</p>}
+        {sdk && !remoteStatus && <p>Fetching rollup provider status...</p>}
+        {remoteStatus && (
+          <p>Next Rollup ID: {remoteStatus.blockchainStatus.nextRollupId}</p>
+        )}
       </header>
     </div>
   );
