ba1210c553096a3969df818a592e3f9234efa0ed
Globallager <72797635+Globallager@users.noreply.github.com>
2022-09-26 11:27:58 +0800

    Delete Unnecessary React Components

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import {
   RollupProviderStatus,
   SdkFlavour,
 } from "@aztec/sdk";
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
@@ -30,18 +29,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         {!sdk && <p>Initialising Aztec SDK...</p>}
         {sdk && !remoteStatus && <p>Fetching rollup provider status...</p>}
         {remoteStatus && (
